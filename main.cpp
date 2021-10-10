e6605af59d7acf911c93eb979f05d00f02706531
TheStrikeM <mr.daniel551177@gmail.com>
2021-10-10 15:35:07 +0500

    Add bubble sort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+using namespace std;
+
+
+template <typename T>
+void strikeSwap(T& a, T& b) {
+    int c = a;
+    a = b;
+    b = c;
+}
+
+void printArray(int arr[], int size) {
+    for (size_t i = 0; i < size; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+//O(n^2)
+void bubbleSort(int arr[], const int size) {
+    for (size_t i = 0; i < size; ++i) {
+        for (size_t j = 0; j < size-1-i; ++j)
+            if (arr[j] > arr[j + 1]) strikeSwap(arr[j], arr[j+1]);
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int arr[5] = { 8,6,7,9,1 };
+    printArray(arr, 5);
+    bubbleSort(arr, 5);
+    printArray(arr, 5);
     return 0;
 }
